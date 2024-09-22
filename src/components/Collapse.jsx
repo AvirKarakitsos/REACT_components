a14fe286944333c54e1fb41a8b64c08fd37ee02b
Avir Karakitsos <avir.karakitsos@gmail.com>
2024-09-22 19:10:20 +0200

    feat: description animation

--- src/components/Collapse.jsx
+++ src/components/Collapse.jsx
@@ -1,9 +1,14 @@
 import styled from "styled-components"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useState } from "react";
+import { useRef, useState } from "react";
+
+const heightDescription = '200px';
+const speed = "1500ms";
 
 const DetailsCollapse = styled.div`
     width: 250px;
+    border-radius: 10px 10px 0 0;
+    overflow: hidden;
 `
 const TitleCollapse = styled.div`
     height: 50px;
@@ -21,31 +26,42 @@ const TitleCollapse = styled.div`
     svg {
         font-size: 22px;
         transform: ${(props) => (props.$active ? 'rotate(-90deg)' : 'rotate(0deg)')};
-        transition: transform 1500ms;
+        transition: transform ${speed};
     }
 `
 const DescriptionCollapse = styled.div`
-    height: 150px;
-    padding: 5px;
+    height: ${(props) => (props.$active ? heightDescription : '0px')};
+    position: relative;
+    padding: 0 5px;
     background-color: pink;
     border-radius: 0 0 10px 10px;
+    transform: ${(props) => (props.$active ? 'translateY(0px)' : `translateY(-${heightDescription})`)};
+    transition: all ${speed};
+    z-index: -1;
+    overflow: hidden;
 
     p {
         margin: 0;
+        padding: 5px 0;
     }
 `
 
 
 function Collapse() {
     const [isRotate, setIsRotate] = useState(false)
+    const divRef = useRef(null)
 
     return (
         <DetailsCollapse className="details">
-            <TitleCollapse className="title" $active={isRotate} onClick={() => setIsRotate(value => !value )}>
+            <TitleCollapse className="title" $active={isRotate} onClick={() => {
+                setIsRotate(value => !value )
+            }
+            } 
+            >
                 <h2>Title</h2>
                 <FontAwesomeIcon icon="fa-solid fa-chevron-left"/>
             </TitleCollapse>
-            <DescriptionCollapse className="description">
+            <DescriptionCollapse className="description" $active={isRotate} ref={divRef}>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus molestias quis aperiam laudantium ipsa neque ducimus consequatur saepe, maiores illo reprehenderit libero, provident atque in, eveniet officia vero labore iure.</p>
             </DescriptionCollapse>
         </DetailsCollapse>
