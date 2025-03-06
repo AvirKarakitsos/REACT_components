f84930bfc9e7d6cea9bd99d856f88adcace61733
Avir Karakitsos <avir.karakitsos@gmail.com>
2025-03-06 17:46:48 +0100

    feat: code for ShowUp component

--- src/components/ShowUp.jsx
+++ src/components/ShowUp.jsx
@@ -1,9 +1,39 @@
+import { useState } from "react"
 import styled from "styled-components"
 
+const ButtonComponent = styled.div`
+    display: flex;
+    column-gap: 15px;
+`
+
+const Container = styled.div`
+    height: 50px;
+    width: 250px;
+    position: relative;
+    overflow: hidden;
+    border: 1px solid red;
+`
+
+const Text = styled.h3`
+    position: absolute;
+    top: ${props => props.$show ? "0px" : "50px"};
+    left: 0;
+    transition: top 1000ms ;
+`
+
 function ShowUp() {
+    const [play,setPlay] = useState(false)
 
     return(
-        <>Test</>
+        <>
+            <ButtonComponent>
+                <button onClick={() => setPlay(true)}>Play</button>
+                <button onClick={() => setPlay(false)}>Reset</button>
+            </ButtonComponent>
+            <Container>
+                <Text $show={play}>Test Title</Text>
+            </Container>
+        </>
     )
 }
 
